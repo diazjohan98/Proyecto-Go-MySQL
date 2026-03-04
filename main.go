94774c36a32beef7c4193b8f40fddee02bdea441
diazjohan98 <vasquez.johan@correounivalle.edu.co>
2026-03-04 07:04:49 -0500

    feat: implement update functionality and complete CRUD application
    - Add edit and update logic in main.go to modify employee records.
    
    - Create editar.tmpl view with a pre-filled Bootstrap form.
    
    - Successfully complete all Create, Read, Update, and Delete operations.

--- main.go
+++ main.go
@@ -32,6 +32,9 @@ func main() {
 
 	http.HandleFunc("/borrar", Borrar)
 
+	http.HandleFunc("/editar", Editar)
+	http.HandleFunc("/actualizar", Actualizar)
+
 	log.Println("Servidor corriendo...")
 
 	http.ListenAndServe(":8080", nil)
@@ -90,6 +93,31 @@ func Inicio(w http.ResponseWriter, r *http.Request) {
 	plantillas.ExecuteTemplate(w, "inicio", arregloEmpleado)
 }
 
+func Editar(w http.ResponseWriter, r *http.Request) {
+	idEmpleado := r.URL.Query().Get("id")
+	fmt.Println(idEmpleado)
+
+	conexionEstablecida := conexionBD()
+
+	registro, err := conexionEstablecida.Query("SELECT * FROM empleados WHERE id=?", idEmpleado)
+
+	empleado := Empleado{}
+	for registro.Next() {
+		var id int
+		var nombre, correo string
+		err = registro.Scan(&id, &nombre, &correo)
+		if err != nil {
+			panic(err.Error())
+		}
+		empleado.Id = id
+		empleado.Nombre = nombre
+		empleado.Correo = correo
+	}
+
+	fmt.Println(empleado)
+	plantillas.ExecuteTemplate(w, "editar", empleado)
+}
+
 func Crear(w http.ResponseWriter, r *http.Request) {
 	plantillas.ExecuteTemplate(w, "crear", nil)
 }
@@ -107,6 +135,24 @@ func Insertar(w http.ResponseWriter, r *http.Request) {
 			panic(err.Error())
 		}
 		insertarRegistros.Exec(nombre, correo)
+
+		http.Redirect(w, r, "/", 302)
+	}
+}
+func Actualizar(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		id := r.FormValue("id")
+		nombre := r.FormValue("nombre")
+		correo := r.FormValue("correo")
+
+		conexionEstablecida := conexionBD()
+
+		modificarRegistros, err := conexionEstablecida.Prepare("UPDATE empleados SET nombre=?,correo=? WHERE id=?")
+
+		if err != nil {
+			panic(err.Error())
+		}
+		modificarRegistros.Exec(nombre, correo, id)
 
 		http.Redirect(w, r, "/", 302)
 	}
